3cc2fbc0b3324b08eeb7851397bb973fd67cb955
peisong <bringtree@qq.com>
2017-10-22 00:30:10 +0800

    finish output_y format

--- main.py
+++ main.py
@@ -23,12 +23,24 @@ del str
 src_stop_word = pwd + '/stopwords' + '/english'
 stop_word_file = util.load(src_stop_word).split('\n')
 
-for i in range(len(sentence)):
+# filter stopping word.
+for i in range(len(sentence)-1):
   str = sentence[i].split()
   sentence[i] = [word for word in str if(word not in stop_word_file)]
   sentence[i] = ' '.join(sentence[i])
 
+# load intent corpora
+src_train = pwd + '/data' + '/atis.train.w-intent.iob'
+intent_file = util.load(src_train).split('\n')
+for i in range(len(intent_file)-1):
+  intent_file[i] = intent_file[i].split()
+  intent_file[i] = intent_file[i][-1]
+del intent_file[len(intent_file)-1]
+
+
+
 vectorizer = CountVectorizer()
 vectorizer.fit(sentence)
 vector = vectorizer.transform(sentence)
-end = vector.toarray()
+input_x = vector.toarray()
+output_y = intent_file
