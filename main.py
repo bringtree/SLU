b2df1a53c96a1f199abfd65ec599b36a0a41cb02
peisong <bringtree@qq.com>
2017-10-22 10:50:01 +0800

    解构代码

--- main.py
+++ main.py
@@ -1,104 +1,36 @@
 import os
 from sklearn.feature_extraction.text import CountVectorizer
-import util
+from util import *
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn import tree
 
-pwd = os.getcwd()
-
-# load atis corpora
-src_train = pwd + '/data' + '/atis.train.w-intent.iob'
-# src_train = pwd + '/data' + '/aaa.iob'
-train_file = util.load(src_train).split()
-train_sentence = []
-str = ''
-for word in train_file:
-  if word == 'BOS':
-    str = ''
-  elif word == 'EOS':
-    train_sentence.append(str)
-  else:
-    str = str + word + ' '
-
-del str
-
-# load stop_word copora
-src_stop_word = pwd + '/stopwords' + '/english'
-stop_word_file = util.load(src_stop_word).split('\n')
-
-# filter stopping word.
-for i in range(len(train_sentence) - 1):
-  str = train_sentence[i].split()
-  train_sentence[i] = [word for word in str if (word not in stop_word_file)]
-  train_sentence[i] = ' '.join(train_sentence[i])
-
-# load intent corpora
-src_train = pwd + '/data' + '/atis.train.w-intent.iob'
-# src_train = pwd + '/data' + '/aaa.iob'
-train_intent_file = util.load(src_train).split('\n')
-for i in range(len(train_intent_file) - 1):
-  train_intent_file[i] = train_intent_file[i].split()
-  train_intent_file[i] = train_intent_file[i][-1]
-del train_intent_file[len(train_intent_file) - 1]
-
+train_sentence, train_intent = load_train_data(src_train='/data/atis.train.w-intent.iob',
+                                               src_stop_word='/stopwords/english')
 # encode train_sentence to word vector
-train_input_x_dict= CountVectorizer()
-train_input_x_dict.fit(train_sentence)
-vector = train_input_x_dict.transform(train_sentence)
+train_input_x_dict = CountVectorizer()
+train_input_x_dict.fit(train_intent)
+vector = train_input_x_dict.transform(train_intent)
 train_input_x = vector.toarray()
 # encode train_intent_file tot intent vector
 train_input_y_dict = CountVectorizer()
-train_input_y_dict.fit(train_intent_file)
-vector = train_input_y_dict.transform(train_intent_file)
+train_input_y_dict.fit(train_intent)
+vector = train_input_y_dict.transform(train_intent)
 train_output_y = vector.toarray()
 
-# load atis corpora
-src_test = pwd + '/data' + '/atis.test.w-intent.iob'
-# src_test = pwd + '/data' + '/bbb.iob'
-test_file = util.load(src_test).split()
-test_sentence = []
-str = ''
-for word in test_file:
-  if word == 'BOS':
-    str = ''
-  elif word == 'EOS':
-    test_sentence.append(str)
-  else:
-    str = str + word + ' '
 
-del str
-
-# load stop_word copora
-src_stop_word = pwd + '/stopwords' + '/english'
-stop_word_file = util.load(src_stop_word).split('\n')
-
-# filter stopping word.
-for i in range(len(test_sentence) - 1):
-  str = test_sentence[i].split()
-  test_sentence[i] = [word for word in str if (word not in stop_word_file)]
-  test_sentence[i] = ' '.join(test_sentence[i])
-
-# load intent corpora
-src_test = pwd + '/data' + '/atis.test.w-intent.iob'
-# src_test = pwd + '/data' + '/bbb.iob'
-test_intent_file = util.load(src_test).split('\n')
-for i in range(len(test_intent_file) - 1):
-  test_intent_file[i] = test_intent_file[i].split()
-  test_intent_file[i] = test_intent_file[i][-1]
-del test_intent_file[len(test_intent_file) - 1]
-
+test_sentence, test_intent = load_test_data(src_test='/data/atis.test.w-intent.iob', src_stop_word='/stopwords/english')
 # encode test_sentence to word vector
-
 vector = train_input_x_dict.transform(test_sentence)
 test_x = vector.toarray()
 # encode test_intent_file tot intent vector
-vector = train_input_y_dict.transform(test_intent_file)
+vector = train_input_y_dict.transform(test_intent)
 test_y = vector.toarray()
+
 
 clf = RandomForestClassifier(criterion='entropy', n_estimators=1, random_state=1, n_jobs=-1)
 clf = clf.fit(train_input_x, train_output_y)
-scores = cross_val_score(clf,train_input_x,train_output_y)
+scores = cross_val_score(clf, train_input_x, train_output_y)
 print(scores.mean())
 
 # scores = cross_val_score(clf,test_x,test_y)
--- util.py
+++ util.py
@@ -1,5 +1,87 @@
+import os
 def load(src):
   temp_file = open(src, "r", encoding='utf-8')
   save_variable = temp_file.read()
   temp_file.close()
   return save_variable
+
+
+def load_train_data(src_train, src_stop_word='/stopwords/english', workspace=None):
+  if workspace == None:
+    pwd = os.getcwd()
+  else:
+    pwd = workspace
+
+  # load atis corpora
+  src = pwd + src_train
+  train_file = load(src).split()
+  train_sentence = []
+  str = ''
+  for word in train_file:
+    if word == 'BOS':
+      str = ''
+    elif word == 'EOS':
+      train_sentence.append(str)
+    else:
+      str = str + word + ' '
+
+  del str
+
+  # load stop_word copora
+  src_stop_word = pwd + src_stop_word
+  stop_word_file = load(src_stop_word).split('\n')
+
+  # filter stopping word.
+  for i in range(len(train_sentence) - 1):
+    str = train_sentence[i].split()
+    train_sentence[i] = [word for word in str if (word not in stop_word_file)]
+    train_sentence[i] = ' '.join(train_sentence[i])
+
+  # load intent corpora
+  src = pwd + src_train
+  train_intent = load(src).split('\n')
+  for i in range(len(train_intent) - 1):
+    train_intent[i] = train_intent[i].split()
+    train_intent[i] = train_intent[i][-1]
+  del train_intent[len(train_intent) - 1]
+  return train_sentence, train_intent
+
+
+def load_test_data(src_test, src_stop_word='/stopwords/english', workspace=None):
+  if workspace == None:
+    pwd = os.getcwd()
+  else:
+    pwd = workspace
+  # load atis corpora
+  src = pwd + src_test
+  test_file = load(src).split()
+  test_sentence = []
+  str = ''
+  for word in test_file:
+    if word == 'BOS':
+      str = ''
+    elif word == 'EOS':
+      test_sentence.append(str)
+    else:
+      str = str + word + ' '
+
+  del str
+
+  # load stop_word copora
+  src_stop_word = pwd + src_stop_word
+  stop_word_file = load(src_stop_word).split('\n')
+
+  # filter stopping word.
+  for i in range(len(test_sentence) - 1):
+    str = test_sentence[i].split()
+    test_sentence[i] = [word for word in str if (word not in stop_word_file)]
+    test_sentence[i] = ' '.join(test_sentence[i])
+
+  # load intent corpora
+  src = pwd + src_test
+  test_intent = load(src).split('\n')
+  for i in range(len(test_intent) - 1):
+    test_intent[i] = test_intent[i].split()
+    test_intent[i] = test_intent[i][-1]
+  del test_intent[len(test_intent) - 1]
+  return test_sentence, test_intent
